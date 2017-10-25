c31bd169c3519addc34636e47371e08afb27f764
Mikael Ahonen <mikael.ahonen@hotmail.com>
2017-10-25 21:47:27 +0300

    Remove group variable from k means.

--- Data Mining/k-means.R
+++ Data Mining/k-means.R
@@ -5,15 +5,11 @@ x <- c(x.1, x.2)
 y.1 <- runif(5,1,3)
 y.2 <- runif(5,2,4)
 y <- c(y.1, y.2)
-
-group.1 <- rep("A", 5)
-group.2 <- rep("B", 5)
-group <- c(group.1, group.2)
   
-observations <- data.frame(x,y,group) 
+observations <- data.frame(x,y) 
 
 #nstart: how many different start centroids
-clusters <- kmeans(observations[,1:2], 2, iter.max=10, nstart=5)
+clusters <- kmeans(observations, 2, iter.max=10, nstart=5)
 
 #Predicted clusters
 clusters$cluster
