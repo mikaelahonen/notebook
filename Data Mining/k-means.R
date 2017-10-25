c327cb298afaaf2b0cacd8a9b604002edfd3c101
Mikael Ahonen <mikael.ahonen@hotmail.com>
2017-10-25 21:42:19 +0300

    2017-20-25 21:43

--- Data Mining/k-means.R
+++ Data Mining/k-means.R
@@ -1,2 +1,39 @@
-#To be done
-#kmeans()
+x.1 <- runif(5,1,3)
+x.2 <- runif(5,2,4)
+x <- c(x.1, x.2)
+
+y.1 <- runif(5,1,3)
+y.2 <- runif(5,2,4)
+y <- c(y.1, y.2)
+
+group.1 <- rep("A", 5)
+group.2 <- rep("B", 5)
+group <- c(group.1, group.2)
+  
+observations <- data.frame(x,y,group) 
+
+#nstart: how many different start centroids
+clusters <- kmeans(observations[,1:2], 2, iter.max=10, nstart=5)
+
+#Predicted clusters
+clusters$cluster
+
+#Cluster centers
+clusters$centers
+
+#cluster sizes
+clusters$size
+
+#iteration that gave the results
+clusters$iter
+
+#whithinss: sum of squares compared to centroids
+clusters$withinss
+
+#betweenss: sum of squares for centroids compared to gloabl mean
+
+#totss: total sum of squares when compared to global mean = withinss + betweenss
+clusters$totss
+
+#This ratio is good measure for total accuracy
+clusters$betweenss / clusters$totss
--- Visualization/ggplot-2/ggplot-geom-point-color-per-variable.R
+++ Visualization/ggplot-2/ggplot-geom-point-color-per-variable.R
@@ -2,9 +2,9 @@ library(ggplot2)
 
 #Daa frame with x, observed and predicted variables.
 data <- data.frame(
-  x=c(1:20),
-  observed=c(1:20+rnorm(20)*2),
-  predicted=c(1:20)
+  x=1:20,
+  observed=1:20+rnorm(20)*2,
+  predicted=1:20
 )
 
 #Point plot per variable with colors
