0574ba4c72113bb5176a421f811b0c0005abf28b
Mikael Ahonen <mikael.ahonen@hotmail.com>
2018-12-21 12:42:06 +0200

    XGBoost notebook example.

--- Python/env.sh
+++ Python/env.sh
@@ -1,5 +1,5 @@
-cd /mnt/c/Users/mikaelah/github/notebook/Python
-export SOME_VARIABLE=SOME_VALUE
+export PYTHON_DIR=/mnt/c/Users/mikaelah/github/notebook/Python
+cd $PYTHON_DIR
 source venv/bin/activate
 pip3 install -r requirements.txt
 #jupyter notebook
--- Python/env_example.sh
+++ Python/env_example.sh
@@ -1,5 +1,5 @@
-cd /mnt/c/Users/maryjane/projects/my_project_folder
-export SOME_VARIABLE=SOME_VALUE
+export PYTHON_DIR=/mnt/c/Users/maryjane/projects/my_project_folder
+cd $PYTHON_DIR
 source venv/bin/activate
 pip3 install -r requirements.txt
 #jupyter notebook
